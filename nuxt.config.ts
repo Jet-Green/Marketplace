1d807854330c60a116d64b6688e6362059b48f9a
Grisha Dzyin <grishadzyin@gmail.com>
2022-07-26 16:12:05 +0400

    add bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,19 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    meta: {
+        link: [
+            {
+                rel: "stylesheet",
+                href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+            },
+        ],
+        script: [
+            {
+                src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js",
+                integrity: "sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa",
+                crossorigin: 'anonymous'
+            }
+        ]
+    },
 })
