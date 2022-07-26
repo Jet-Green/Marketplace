109e62c008e4e508979c5e21f414b77bf6b5f8fe
Grisha Dzyin <grishadzyin@gmail.com>
2022-07-26 14:44:11 +0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Html>
+
+        <Head>
+            <Title>Marketplace</Title>
+            <!-- <Meta name="description" content="My page is cool" /> -->
+            <!-- <Link rel="preload" href="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png" as="script" /> -->
+        </Head>
+
+        </Html>
+
+        <h1>welcome</h1>
+    </div>
+</template>
