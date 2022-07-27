e62bc04f9fae470946f91ba85b60345a6ce10ddb
Grisha Dzyin <grishadzyin@gmail.com>
2022-07-27 16:37:59 +0400

    add antd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,19 @@
 import { defineNuxtConfig } from 'nuxt'
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    meta: {
-        link: [
-            {
-                rel: "stylesheet",
-                href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
-            },
+
+    vite: {
+        plugins: [
+            Components({
+                // add option {resolveIcons: true} as parameter for resolving problem with icons
+                resolvers: [AntDesignVueResolver()],
+            }),
         ],
-        script: [
-            {
-                src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js",
-                integrity: "sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa",
-                crossorigin: 'anonymous'
-            }
-        ]
+        // @ts-expect-error: Missing ssr key
+        ssr: {
+            noExternal: ['moment', 'compute-scroll-into-view', 'ant-design-vue', '@ant-design/icons-vue'],
+        },
     },
 })
